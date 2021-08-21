7be0dd9d426ecd28360d0c9d942ed9f86fe71970
muriloPereira006 <muriloedu006@gmail.com>
2021-08-21 01:38:01 -0300

    preciso incluir os users

--- controller/blogPostController.js
+++ controller/blogPostController.js
@@ -5,18 +5,12 @@ require('dotenv').config();
 const createPost = async (req, res) => {
   try {
     const { title, content, categoryIds } = req.body;
-    // const { email } = req.user;
     const token = req.headers.authorization;
-
-    console.log("token: ", token);
 
     if (!token) return res.status(401).json({ message: 'Token not found' });
     const { data } = jwt.verify(token, process.env.JWT_SECRET);
 
-    console.log('CONSOLADO: ', JSON.stringify(data));
     const user = await Users.findOne({ where: { email: data } });
-    console.log('CONSOLADO USER: ', user);
-    console.log('CONSOLADO DATAVALEUS: ', user.dataValues.id);
     const userId = user.dataValues.id;
 
     const post = await BlogPosts.create({ userId, title, content, categoryIds });
@@ -26,4 +20,18 @@ const createPost = async (req, res) => {
   }
 };
 
-module.exports = { createPost };
+const getAllPost = async (req, res) => {
+  try {
+    const token = req.headers.authorization;
+
+    if (!token) return res.status(401).json({ message: 'Token not found' });
+    jwt.verify(token, process.env.JWT_SECRET);
+
+    const post = await BlogPosts.findAll();
+    return res.status(200).json(post);
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
+
+module.exports = { createPost, getAllPost };
